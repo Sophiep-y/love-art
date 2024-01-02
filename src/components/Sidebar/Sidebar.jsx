bbb646832c7ff9ea24cb32b51e1db2b0f7e2c874
Umesh Shrestha <stha.ums@gmail.com>
2024-01-02 14:55:14 -0500

    redirect to recommends on close sidebar on pages that was navigated from sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import {menuItem} from "../Header/menuItem";
 import {CrossIcon} from "../../assets/svg/cross-icon";
 import {Footer} from "../Footer/Footer";
-import React, {useCallback, useState} from "react";
+import React, {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {HamburgerIcon} from "../../assets/svg";
 
@@ -38,38 +38,37 @@ const SideBar = ({icon, useExitSidebar}) => {
     };
 
 
-    const onClickIcon = useCallback(
-        () => {
-            try {
-                if (isExitNavigation) {
-                    console.log('go to recommend');
-                    navigate('/recommends');
+    const onClickIcon = () => {
+        try {
+            if (isExitNavigation) {
+                navigate('/recommends');
+            } else {
+                if (useExitSidebar) {
+                    console.log("22")
+                    setIsHidden(!isHidden)
+                    setTimeout(() => {
+                        setIsExitNavigation(true)
+                    }, 300)
+
                 } else {
-                    if (useExitSidebar) {
-                        console.log("22")
-                        setIsHidden(!isHidden)
-                        setTimeout(() => {
-                            setIsExitNavigation(true)
-                        }, 300)
-
-                    } else {
-                        toggleSideBar();
-                    }
-                    console.log('what')
+                    toggleSideBar();
                 }
-
-            } catch (e) {
-                console.log(e)
+                console.log('what')
             }
-        },
-        [isExitNavigation, navigate, useExitSidebar, isHidden],
-    );
 
+        } catch (e) {
+            console.log(e)
+        }
+    };
 
     return (<div>
-        <div onClick={onClickIcon} className="cursor-pointer">
-            {icon ? icon : <HamburgerIcon/>}
+        <div onClick={(e) => {
+            e.stopPropagation();
+            onClickIcon();
+        }} className="cursor-pointer">
+            {icon ?? <HamburgerIcon/>}
         </div>
+        {/*overlay*/}
         {(!isCollapsed || isExitNavigation) ? <div
             className="fixed top-0 left-0 w-full h-full bg-black opacity-50 z-10"
 
@@ -85,9 +84,14 @@ const SideBar = ({icon, useExitSidebar}) => {
             }}
         ></div> : null}
 
+        {/*sidebar*/}
         <div
             className={
-                useExitSidebar ? isExitNavigation ? `absolute bg-white z-10 shadow-2xl shadow-modalShadowColor  py-5 w-2/3 h-full top-0 pt-0.5 pl-5 left-1/3 transform transition-all duration-300 ease-in-out -translate-x-0` : `${isHidden ? 'hidden' : 'block'}  absolute bg-white z-10 shadow-2xl shadow-modalShadowColor  py-5  w-full h-full top-0 pt-0.5 pl-5  left-0   transform transition-all duration-300 ease-in-out  translate-x-0 ` : `absolute bg-white z-10 shadow-2xl shadow-modalShadowColor  py-5   ${isNavigation ? 'w-full' : 'w-2/3'}  h-full top-0 pt-0.5 pl-5 ${isNavigation ? 'left-0' : 'left-1/3'}  transform transition-all duration-300 ease-in-out ${isCollapsed ? 'translate-x-full' : isNavigation ? 'translate-x-0' : '-translate-x-0'}`
+                useExitSidebar ?
+                    isExitNavigation ?
+                        `absolute bg-white z-10 shadow-2xl shadow-modalShadowColor  py-5 w-2/3 h-full top-0 pt-0.5 pl-5 left-1/3 transform transition-all duration-300 ease-in-out -translate-x-0` :
+                        `${isHidden ? 'hidden' : 'block'}  absolute bg-white z-10 shadow-2xl shadow-modalShadowColor  py-5  w-full h-full top-0 pt-0.5 pl-5  left-0   transform transition-all duration-300 ease-in-out  translate-x-0 `
+                    : `absolute bg-white z-10 shadow-2xl shadow-modalShadowColor  py-5   ${isNavigation ? 'w-full' : 'w-2/3'}  h-full top-0 pt-0.5 pl-5 ${isNavigation ? 'left-0' : 'left-1/3'}  transform transition-all duration-300 ease-in-out ${isCollapsed ? 'translate-x-full' : isNavigation ? 'translate-x-0' : '-translate-x-0'}`
             }
         >
             <div className="flex flex-col">
@@ -100,7 +104,9 @@ const SideBar = ({icon, useExitSidebar}) => {
             </div>
             <div
                 className="absolute top-4 right-4 cursor-pointer"
-                onClick={toggleSideBar}
+                onClick={(e) => {
+                    isExitNavigation ? onClickIcon() : toggleSideBar();
+                }}
             >
                 <CrossIcon/>
             </div>
